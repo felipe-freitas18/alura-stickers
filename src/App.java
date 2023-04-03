766d9c1735c5cf25abe0283c4662ab1ad6e1c4a9
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2023-04-03 18:35:47 -0300

    aula 03 aplicando POO e refatorando

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
 
@@ -14,28 +8,24 @@ public class App {
 		
 		//fazer uma conexão HTTP e buscar os top 250 filmes
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-		URI endereco = URI.create(url);
-		var client =  HttpClient.newHttpClient();
-		var request =  HttpRequest.newBuilder(endereco).GET().build();
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-		String body = response.body();
+		String urlNasa = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
 		
-		//extrair são os dados que interessam (titulo, poster, classificação)
-		var parser = new JsonParser();
-		List<Map<String, String>> listaDeFilmes = parser.parse(body);	
+		ClienteHttp http = new ClienteHttp();
+		String json = http.buscaDados(urlNasa);
+		
+		// exibir e manipular os dados
+		ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+		List<Conteudo> conteudos  = extrator.extraiConteudos(json);
 		
 		var geradora = new GeradoraDeFigurinhas();
-		// exibir e manipular os dados
-		for(Map<String,String> filme: listaDeFilmes) {
-			InputStream inputStream = new URL(filme.get("image")).openStream();
-			String titulo = filme.get("title");
-			String nomeArquivo = titulo.replace(":", "-")  + ".png";
-			geradora.cria(inputStream, nomeArquivo);
+
+		for( Conteudo conteudo : conteudos ) {
 			
-			System.out.println("\u001b[35m"+titulo+"\u001b[m");
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
-			//adicionar nessa linha posteriormente o sysout contendo os icons de estrelas
+			InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+			String nomeArquivo = conteudo.getTitulo().replace(":", "-")  + ".png";
+			geradora.cria(inputStream, nomeArquivo);
+	
+			System.out.println(conteudo.getTitulo());
 			System.out.println();
 		}
 	}
